8a007441f432e29795fef7ad1bdbcc9cb180e92d
Pedro Claros <pedroclaros@hotmail.com>
2022-07-30 23:31:55 -0300

    add users seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  #devise :database_authenticatable, :registerable,
-  #       :recoverable, :rememberable, :validatable
-  devise :database_authenticatable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  #devise :database_authenticatable, :validatable
 
   belongs_to :company
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,41 @@
 
 puts "\n== Seeding the database with fixtures =="
 system("bin/rails db:fixtures:load")
+
+
+
+#--------------------------------------------------------------------------
+
+kpmg_company = Company.new(
+  name: "Kpmg",
+)
+kpmg_company.save!
+
+deloitte_company = Company.new(
+  name: "Deloitte",
+)
+deloitte_company.save!
+
+
+kpmg_user = User.new(
+  email: "manager@kpmg.com",
+  password: "123456",
+  company_id:  kpmg_company["id"],
+)
+kpmg_user.save!
+
+
+kpmg_user2 = User.new(
+  email: "account@kpmg.com",
+  password: "123456",
+  company_id:  kpmg_company["id"],
+)
+kpmg_user2.save!
+
+
+deloitte_user = User.new(
+  email: "manager@deloitte.com",
+  password: "123456",
+  company_id:  deloitte_company["id"],
+)
+deloitte_user.save!
