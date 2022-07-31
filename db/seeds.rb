ca3154c7a3c70c33bc3fc2e2a6b28afce3c3acd7
Pedro Claros <pedroclaros@hotmail.com>
2022-07-31 18:22:11 -0300

    added some styles and home introduction

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,8 @@ system("bin/rails db:fixtures:load")
 
 #--------------------------------------------------------------------------
 
+#Create companies
+
 kpmg_company = Company.new(
   name: "Kpmg",
 )
@@ -24,6 +26,8 @@ deloitte_company = Company.new(
 deloitte_company.save!
 
 
+#Create user
+#Use this user to show seeds
 kpmg_user = User.new(
   email: "manager@kpmg.com",
   password: "123456",
@@ -31,7 +35,7 @@ kpmg_user = User.new(
 )
 kpmg_user.save!
 
-
+#Use this user for real time sessions, at the same time with manager@kpmg.com
 kpmg_user2 = User.new(
   email: "account@kpmg.com",
   password: "123456",
@@ -39,10 +43,33 @@ kpmg_user2 = User.new(
 )
 kpmg_user2.save!
 
-
+#another user
 deloitte_user = User.new(
   email: "manager@deloitte.com",
   password: "123456",
   company_id:  deloitte_company["id"],
 )
 deloitte_user.save!
+
+
+#Seeds for Manager KPMG user, manager@kpmg.com
+#Service 1
+account_services = Quote.create(name:'Account Services - Accenture', company_id: kpmg_company["id"])
+
+account_services_date = LineItemDate.create(quote_id: account_services["id"], date: Date.new(2022,8,8))
+
+LineItem.create(line_item_date_id: account_services_date["id"], name:'Stock revision', description:'control stock outflows from the warehouse in brazil', quantity: 1, unit_price: 5000,)
+
+#Product 2
+account_product = Quote.create(name:'Software Product - Accenture', company_id: kpmg_company["id"])
+
+account_product_date = LineItemDate.create(quote_id: account_product["id"], date: Date.new(2022,8,10))
+
+LineItem.create(line_item_date_id: account_product_date["id"], name:'Software for Warehouse', description:'Software for stock control, 2 licenses', quantity: 2, unit_price: 10000,)
+
+#Service 3
+account_services1 = Quote.create(name:'Training - Deloitte', company_id: kpmg_company["id"])
+
+account_services1_date = LineItemDate.create(quote_id: account_services1["id"], date: Date.new(2022,10,8))
+
+LineItem.create(line_item_date_id: account_services1_date["id"], name:'Commercial Training', description:'commercial training for the sales force, 7 sellers', quantity: 7, unit_price: 3000,)
